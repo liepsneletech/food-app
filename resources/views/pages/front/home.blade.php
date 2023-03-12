1b4cc98cfea39cdefc581adfa33d308c5c25b52c
liepsnele <liepsnele.tech@gmail.com>
2023-03-13 01:07:34 +0200

    update

--- resources/views/pages/front/home.blade.php
+++ resources/views/pages/front/home.blade.php
@@ -12,14 +12,14 @@
                     ac diam sit amet.</p>
                 <a href="{{ route('restaurants-index') }}" class="primary-btn">Restoranai</a>
             </div>
-            <a href="#meals"
+            <a href="#popular-meals"
                 class="text-3xl opacity-85 text-cyan-800 absolute right-[50%] translate-x-1/2 bottom-8 animate-pulse"><i
                     class="fa-solid fa-angles-down"></i></a>
         </div>
     </section>
 
     {{-- products section --}}
-    <section class="products container" id="products">
+    <section class="products container" id="popular-meals">
         <h2 class="primary-heading text-center">Populiariausi patiekalai</h2>
     </section>
 
@@ -85,6 +85,52 @@
                 <p class="text-gray-600">Pellentesque in ipsum id orci porta dapibus. Lorem ipsum dolor sit amet,
                     consectetur adipiscing elit. Sed porttitor lectus nibh.</p>
             </div>
+        </div>
+    </section>
+
+    {{-- contact us section --}}
+    <section class="items bg-gray-100" id="contacts">
+        <div class="container">
+            <h2 class="primary-heading text-center">Susisiekite</h2>
+
+            <form method="post" class="w-1/2 mx-auto">
+                @csrf
+
+                <x-form.label for="name" :value="__('Vardas')" />
+                <x-form.input id="name" class="block mt-1 w-full mb-4 text-gray-500" type="text" name="name"
+                    :value="old('name')" />
+
+                @error('name')
+                    <p class="text-white bg-red-500 rounded-lg py-1 px-4 text-sm mt-3 mb-5">
+                        {{ $message }}
+                    </p>
+                @enderror
+
+                <x-form.label for="email" :value="__('El. paštas')" />
+                <x-form.input id="email" class="block mt-1 w-full mb-4 text-gray-500" type="email" name="email"
+                    :value="old('email')" />
+
+                @error('email')
+                    <p class="text-white bg-red-500 rounded-lg py-1 px-4 text-sm mt-3 mb-5">
+                        {{ $message }}
+                    </p>
+                @enderror
+
+                <x-form.label for="review_text" :value="__('Žinutė:')" class="mt-3" />
+                <textarea name="review_text" id="review_text" rows="3"
+                    class="w-full rounded-2xl py-2 border-gray-300 focus:border-gray-300 focus:ring
+                focus:ring-cyan-100 text-gray-500">{{ old('review_text') }}</textarea>
+
+                @error('review_text')
+                    <p class="text-white bg-red-500 rounded-lg py-1 px-4 text-sm mt-3 mb-5">
+                        {{ $message }}
+                    </p>
+                @enderror
+
+                <button class="mt-4 mb-10 secondary-btn" type="submit">
+                    Nusiųsti
+                </button>
+            </form>
         </div>
     </section>
 </x-front-layout>
