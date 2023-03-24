034af4948d0ddcb39026c11c93e4d11e2d9f7aae
cinamona <levickaite.m@gmail.com>
2023-03-24 03:14:54 +0200

    build done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/front/app.css",
+                "resources/css/back/app.css",
+                "resources/js/front/app.js",
+                "resources/js/back/app.js",
+            ],
             refresh: true,
         }),
     ],
